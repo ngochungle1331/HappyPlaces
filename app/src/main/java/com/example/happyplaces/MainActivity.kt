28c315edb64c40e8d12c5b5edd3d184b15b2342f
Ngoc Hưng Lê <ngochungle1331@gmail.com>
2023-01-15 21:26:54 +0700

    add HappyPlace activity and use dimen string color

---
+++ app/src/main/java/com/example/happyplaces/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.happyplaces
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.happyplaces.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.fabAddHappyPlace?.setOnClickListener {
+            val intent = Intent(this@MainActivity, AddHappyPlaceActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
+    }
+}
